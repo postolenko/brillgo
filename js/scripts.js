045c3f5292cf39abf984b32dad4ed2b77411c835
postolenko <denis.postolenko@gmail.com>
2022-08-31 01:02:38 +0300

    Brillgo

--- js/scripts.js
+++ js/scripts.js
@@ -54,4 +54,22 @@ $(document).ready(function() {
         });
     }
 
+    // ----------------
+
+    $(".tab_link").click(function(e) {
+      parent = $(this).closest(".tab_list");
+      parent.find(".tab_link").removeClass("active");
+      $(this).addClass("active");
+    });
+
+    // ----------------
+
+    $("#callbackBtn").on("click", function(e) {
+      e.preventDefault();
+      $("#tab_1").trigger("click");
+      parent = $(this).closest(".tabs_wrapp");
+      parent.find(".tab_link").removeClass("active");
+      $("[for='tab_1']").addClass("active");
+    });
+
 });
